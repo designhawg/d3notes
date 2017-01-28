d7fa86ddb3e4d4fa4d0a5faf80b67e5dbe878ed3
designhawg <designhawg@PicassoAir.local>
2017-01-28 00:49:19 -0600

    bar chart

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// add axis to the chart
-// we'll adjust the margins in this exercise
 var margin = { top: 10, right: 20, bottom: 30, left: 40 }
 
 var width = 425 - margin.left - margin.right;
@@ -13,52 +11,53 @@ var svg = d3.select('.chart')
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.append('rect')
-  .attr('width', width)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green');
+// beginning of exercise17
 
+// first add data
 
-// beginning of exercise16
+var data = [
+  { score: 63, subject: 'Math' },
+  { score: 98, subject: 'Journalism' },
+  { score: 80, subject: 'Foosball' }
+];
 
-// create the scale for vertical axis
 var yScale = d3.scaleLinear()
-  // sets the numerical range
-  // change domain to illustrate that d3 auto updates
-  // for percentages .domain should be 0-1
   .domain([0, 100])
   .range([height, 0]);
 
 var yAxis = d3.axisLeft(yScale);
-// to change tick marks add .ticks
-// var yAxis = d3.axisLeft(yScale).ticks(5);
-// for percentages, add an additional argument
-// var yAxis = d3.axisLeft(yScale).ticks(5, '%');
-// for exact values
-// var yAxis = d3.axisLeft(yScale).ticksValues([5,10,17,100]);
-
-// append vertical axis
 svg.call(yAxis)
 
-// create the scale for horizontal axis
-var xScale = d3.scaleTime()
-  .domain([new Date(2016, 0, 1), new Date(2017, 0, 1)])
-  // for date timeline by hour
-  // .domain([new Date(2016, 0, 1, 6), new Date(2016, 0, 1, 12)])
-
+// adjust to scaleBand
+var xScale = d3.scaleBand()
+  // add padding later
+  .paddingInner(0.2)
+  .paddingOuter(0.5)
+  // add datamap instead of time
+  .domain(data.map(d => d.subject))
   .range([0, width]);
 
 var xAxis = d3.axisBottom(xScale)
-  .ticks(5);
-  // additional tick options
-  // .tickSizeInner(10)
-  // .tickSizeOuter(20)
-  // .tickPadding(15)
+  .ticks(5)
+  .tickSize(10)
+  .tickPadding(5);
 
-// append horizontal axis
 svg
-  // first we need to move the (g) element down
   .append('g')
     .attr('transform', `translate(0, ${height})`)
   .call(xAxis);
+  // later add this to rotate labels
+  // .selectAll('text')
+  // .style('text-anchor', 'end')
+  // .attr('transform', 'rotate(-45)');
+
+
+// add data via datajoin
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', d => xScale(d.subject))
+  .attr('y', d => yScale(d.score))
+  .attr('width', d => xScale.bandwidth())
+  .attr('height', d => height - yScale(d.score));
